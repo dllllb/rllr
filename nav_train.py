e7a421176467df709e853353529c054a38b89e0d
Dmitri Babaev <dmitri.babaev@gmail.com>
2019-06-13 10:12:13 +0300

    pgrad demo fix

--- nav_train.py
+++ nav_train.py
@@ -11,14 +11,14 @@ torch.manual_seed(1)
 
 explore_policy = RandomActionPolicy(env)
 
+te = TrajectoryExplorer(env, explore_policy, n_episodes=500, n_steps=3)
+tasks = generate_train_trajectories(te, 3, .5)
+
 nav_nn = pgrad.ConvPolicy(env)
 
 np_optimizer = torch.optim.Adam(nav_nn.parameters(), lr=0.01)
 np_updater = pgrad.PGUpdater(np_optimizer, gamma=.99)
 policy = pgrad.NNPolicy(nav_nn, np_updater)
-
-te = TrajectoryExplorer(env, explore_policy, n_episodes=500, n_steps=3)
-tasks = generate_train_trajectories(te, 3, .5)
 
 nt = NavigationTrainer(env, policy, n_steps_per_episode=10, state_dist=ssim_dist)
 nt(tasks)
--- pgrad_demo.py
+++ pgrad_demo.py
@@ -11,6 +11,6 @@ torch.manual_seed(1)
 nn = pgrad.MLPPolicy(env)
 optimizer = optim.Adam(nn.parameters(), lr=0.01)
 updater = pgrad.PGUpdater(optimizer, gamma=.99)
-policy = pgrad.NNPolicy(pgrad.MLPPolicy(env), updater)
+policy = pgrad.NNPolicy(nn, updater)
 
 pgrad.train_loop(env=env, policy=policy, n_eposodes=1000)
