correct bag with nan gradients

navigation_models.py
@@ -2,7 +2,9 @@ import gym
 import torch
 import numpy as np
 from torch import nn
+import torch.nn.functional as F
 from constants import *
+import cv2
 
 
 def prepare_state(state):
@@ -14,6 +16,35 @@ def prepare_state(state):
     elif len(state.shape) == 2:
         state = state.view(1, 1, *state.shape)
     return state.to(DEVICE)
+
+class ConvBody(nn.Module):
+
+    def __init__(self, env):
+        super().__init__()
+        img_height, img_width, img_channels = env.observation_space.shape
+
+        self.convs = nn.ModuleList([
+            nn.Conv2d(img_channels, 32, kernel_size=8, stride=4),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.Conv2d(64, 64, kernel_size=2, stride=2),
+            nn.Conv2d(64, 1, kernel_size=1, stride=1),
+        ])
+
+        #HooksF = [Hook(conv, f'conv_{i+1}') for i, conv in enumerate(self.convs)]
+        #HooksB = [Hook(conv, f'conv_{i+1}', backward=True) for i, conv in enumerate(self.convs)]
+
+        with torch.no_grad():
+            self.norms = []
+            x = torch.randn(1, img_channels, img_height, img_width)
+            for conv in self.convs:
+                x = conv(x)
+                self.norms.append(nn.LayerNorm(normalized_shape=x.size()[1:]))
+            self.norm = nn.ModuleList(self.norms)
+
+    def forward(self, x):
+        for i, conv, norm in zip(range(len(self.convs)), self.convs, self.norms):
+            x = F.relu(norm(conv(x)))
+        return x
 
 
 def state_embed_block(img_channels):
@@ -35,9 +66,9 @@ class ConvNavPolicy(nn.Module):
 
         img_height, img_width, img_channels = env.observation_space.shape
 
-        self.conv = state_embed_block(img_channels)
+        self.conv = ConvBody(env) #state_embed_block(img_channels)
 
-        self.conv_target = state_embed_block(img_channels)
+        self.conv_target = ConvBody(env) #state_embed_block(img_channels)
 
         o = self.conv(torch.zeros(1, img_channels, img_height, img_width))
         conv_out_size = int(np.prod(o.size()))
@@ -47,50 +78,26 @@ class ConvNavPolicy(nn.Module):
             nn.LayerNorm([128]),
             nn.ReLU(),
             nn.Linear(128, env.action_space.n),
-            #nn.Softmax(dim=-1)
+            nn.LogSoftmax(dim=-1)
         )
-        self.act = nn.LogSoftmax(dim=-1)
+        #HooksF = [Hook(layer, f'head_{layer.__repr__()}') for layer in self.fc]
+        #HooksB = [Hook(layer, f'head_{layer.__repr__()}', backward=True) for layer in self.fc]
+
+        
 
     def forward(self, state):
-        current_state, desired_state = state
-        current_state = prepare_state(current_state)
-        desired_state = prepare_state(desired_state)
-        current_state = (current_state - current_state.min())/(current_state.max() - current_state.min())
-        desired_state = (desired_state - desired_state.mean())/(desired_state.max() - desired_state.min())
-
-        if torch.isnan(current_state).any().item():
-            print(f'+++++++++++++++++nan in current_state values +++++++++++++++++++\n')
-
-        if torch.isinf(current_state).any().item():
-            print(f'+++++++++++++++++infinity in current_state  values +++++++++++++++++++\n')
-
-        if torch.isnan(desired_state).any().item():
-            print(f'+++++++++++++++++nan in desired_state values +++++++++++++++++++\n')
-
-        if torch.isinf(desired_state).any().item():
-            print(f'+++++++++++++++++infinity in desired_state  values +++++++++++++++++++\n')
+        current_state_, desired_state_ = state
+        current_state = prepare_state(current_state_)
+        desired_state = prepare_state(desired_state_)
+        #current_state = (current_state - current_state.min())/(current_state.max() - current_state.min())
+        #desired_state = (desired_state - desired_state.mean())/(desired_state.max() - desired_state.min())
 
         conv_out = self.conv(current_state).view(current_state.size(0), -1)
         conv_target_out = self.conv_target(desired_state).view(desired_state.size(0), -1)
         #h = torch.cat((conv_out, conv_target_out), dim=1)
         h = conv_target_out - conv_out
 
-        if torch.isnan(h).any().item():
-            print(f'+++++++++++++++++nan in conv layer values +++++++++++++++++++\n')
-
-        if torch.isinf(h).any().item():
-            print(f'+++++++++++++++++infinity in conv layer  values +++++++++++++++++++\n')
-
         ap = self.fc(h)
-
-        if torch.isnan(ap).any().item():
-            print(f'+++++++++++++++++nan in linear layer values +++++++++++++++++++\n')
-
-        if torch.isinf(ap).any().item():
-            print(f'+++++++++++++++++infinity in linear layer  values +++++++++++++++++++\n')
-
-
-        ap = self.act(ap)
         return ap
 
 class GoalConvNavPolicy(nn.Module):

navigation_policy_demo.py
@@ -28,12 +28,12 @@ torch.manual_seed(1)
 
 explore_policy = RandomActionPolicy(env)
 
-te = TrajectoryExplorer(env, explore_policy, n_steps=50, n_episodes=50)
+te = TrajectoryExplorer(env, explore_policy, n_steps=150, n_episodes=50)
 tasks = generate_train_trajectories(te, n_initial_points=3, take_prob=.5)
 
 nav_nn = ConvNavPolicy(env)
 nav_nn.to(DEVICE)
-np_optimizer = torch.optim.Adam(nav_nn.parameters(), lr=0.0001)
+np_optimizer = torch.optim.Adam(nav_nn.parameters(), lr=0.001)
 np_updater = PGUpdater(np_optimizer, gamma=.9)#99)
 policy = NNPolicy(nav_nn, np_updater)
 

pgrad.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from learner import Updater
+from constants import *
 
 
 class PGUpdater(Updater):
@@ -10,19 +11,30 @@ class PGUpdater(Updater):
         
     def __call__(self, context_hist, state_hist, reward_hist):
         R = 0
-        rewards = []
+        rewards_ = []
 
         for r in reward_hist[::-1]:
             R = r + self.gamma * R
-            rewards.insert(0, R)
+            rewards_.insert(0, R)
 
-        rewards = torch.FloatTensor(rewards)
-        rewards = (rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
+        rewards = torch.FloatTensor(rewards_)
+        if rewards.size(0) > 1: # or we get nan values
+            rewards = (rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
 
         loss = []
         for log_prob, reward in zip(context_hist, rewards):
             loss.append((-log_prob * reward).view(1))
         loss = torch.cat(loss).sum()
+
+        if is_nan(loss, 'loss'):
+            print('===')
+            print('-----------------------------------------------------------')
+            print(rewards_)
+            print('-----------------------------------------------------------')
+            print(rewards)
+            print('-----------------------------------------------------------')
+            print('===')
+            
 
         self.optimizer.zero_grad()
         loss.backward()

policy.py
@@ -3,6 +3,7 @@ from torch.distributions import Categorical
 from learner import Learner, Updater, BufferedLearner
 import torch.nn as nn
 import torch
+from constants import *
 
 
 class Policy(Learner):
@@ -30,19 +31,17 @@ class NNPolicy(BufferedLearner, Policy):
     def __call__(self, state):
         take_probs = self.model(state)
 
-        if torch.isnan(take_probs).any().item():
-            print(f'+++++++++++++++++nan in take probs values {take_probs}+++++++++++++++++++\n')
-
-        if torch.isinf(take_probs).any().item():
-            print(f'+++++++++++++++++infinity in take probs values {take_probs}+++++++++++++++++++\n')
-
-        if take_probs.sum().item() == 0:
-            print(f'+++++++++++++++++sum = 0 in take probs values {take_probs}+++++++++++++++++++\n')
+        is_nan(take_probs, 'probs')
         
         #c = Categorical(take_probs) # with softmax activation
         c = Categorical(logits=take_probs)
         action = c.sample()
-        return action.item(), c.log_prob(action).view(1)
+
+        log_p = c.log_prob(action).view(1)
+        if is_nan(log_p, 'log_p'):
+            print(f'lop_prob is Nan for take_probs {take_probs}, action {action.item()}')
+
+        return action.item(), log_p
 
 
 class NNPolicyAV(BufferedLearner, Policy):