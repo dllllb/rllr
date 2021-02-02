491f711345dbbe7d2799dc1986570c7a141314b1
Nikita Ovsov <OvsovNikita@gmail.com>
2021-02-02 21:34:00 +0300

    flatten from wrapper to nn; cosmetic

---
+++ gym_minigrid_navigation/utils.py
@@ -0,0 +1,20 @@
+import base64
+import glob
+import io
+from IPython.display import HTML
+from IPython import display
+import numpy as np
+
+
+def show_video():
+    mp4list = glob.glob('video/*.mp4')
+    if len(mp4list) > 0:
+        mp4 = mp4list[0]
+        video = io.open(mp4, 'r+b').read()
+        encoded = base64.b64encode(video)
+        display.display(HTML(data='''<video alt="test" autoplay 
+                loop controls style="height: 400px;">
+                <source src="data:video/mp4;base64,{0}" type="video/mp4" />
+             </video>'''.format(encoded.decode('ascii'))))
+    else:
+        print("Could not find video")
