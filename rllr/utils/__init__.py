90259c479e734959ea12695744be2a2a7e7ded15
ivannz <ivannz@yandex.ru>
2021-04-16 13:01:36 +0300

    rename utils to logger

--- rllr/utils/__init__.py
+++ rllr/utils/__init__.py
@@ -1,6 +1,6 @@
 from .common import convert_to_torch, switch_reproducibility_on
 
 from .config import get_conf
-from .utils import init_logger
+from .logger import init_logger
 
 # from .plotting import display_stats  # do not import right away
