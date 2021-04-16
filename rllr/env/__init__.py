aa8435c82a623a9472a46787ff6f429e34bc14fb
ivannz <ivannz@yandex.ru>
2021-04-16 13:32:01 +0300

    rename env.env to env.wrappers

--- rllr/env/__init__.py
+++ rllr/env/__init__.py
@@ -1,15 +1,15 @@
-from .environments import NavigationGoalWrapper
-from .environments import RandomGoalWrapper
-from .environments import FromBufferGoalWrapper
-from .environments import SetRewardWrapper
-from .environments import FullyRenderWrapper
+from .wrappers import NavigationGoalWrapper
+from .wrappers import RandomGoalWrapper
+from .wrappers import FromBufferGoalWrapper
+from .wrappers import SetRewardWrapper
+from .wrappers import FullyRenderWrapper
 
 from .rewards import ExplicitPosReward
 from .rewards import SparsePosReward
 from .rewards import SparseStateReward
 from .rewards import ExplicitStepsAmount
 
-from .environments import navigation_wrapper
-from .environments import visualisation_wrapper
+from .wrappers import navigation_wrapper
+from .wrappers import visualisation_wrapper
 
 from .rewards import get_reward_function
