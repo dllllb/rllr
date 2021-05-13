f0411d26f8125dba617ff7289dadd3238581ed5c
morlov <ormian@mail.ru>
2021-05-13 17:31:38 +0300

    Small typo correct

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         'rllr.algo',  # essential RL learning algorithm
         'rllr.env',  # environments and wrappers
         'rllr.models',  # policies and models
-        'rllr.buffer',  # buffer buffer
+        'rllr.buffer',  # replay buffer
         'rllr.utils',  # utilities
         'rllr.utils.plotting',  # visualization
         'rllr.env.gym_minigrid_navigation',  # namespace only
