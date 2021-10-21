0d52849450050ea8f39c17938a91229a8c15a6bd
Ruslan Kostoev <WeaselCMC@users.noreply.github.com>
2021-10-21 15:04:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'pyhocon',            # utils.config :: get_conf
         'jupyter',            # utils.plotting :: IPython
         'gym_minigrid',       # git+https://github.com/maximecb/gym-minigrid.git
-        'git+https://github.com/WeaselCMC/miniwob-plusplus.git',
+        'miniwob @ git+https://github.com/WeaselCMC/miniwob-plusplus.git',
         'stable_baselines3',  # master_worker_*.py
         'scipy',              # environments.py :: scipy.stats
         'tqdm'
