f16afda0aaffd4306f35e3f1017ef295ab8c5a4b
Dmitry Sorokin <dmitrii.sorokin@phystech.edu>
2022-01-25 12:30:38 +0300

    try montezuma with framestack encoder

---
+++ experiments/montezuma/enjoy.py
@@ -0,0 +1,69 @@
+import logging
+
+import numpy as np
+import torch
+
+from argparse import ArgumentParser
+from pyhocon.config_parser import ConfigFactory
+from tqdm import trange
+
+
+from rllr.env.vec_wrappers import make_vec_envs
+from rllr.utils.logger import init_logger
+import torch.nn as nn
+from einops import rearrange
+
+
+logger = logging.getLogger(__name__)
+
+
+def main(args):
+    if args.mode == 'rnd_ppo':
+        from train_rnd_gru_ppo import gen_env_with_seed
+        config = ConfigFactory.parse_file('minigrid/conf/minigrid_rnd_ppo.hocon')
+        agent_path = 'artifacts/models/minigrid_rnd_ppo.p'
+
+    agent = torch.load(config['outputs.path'], map_location='cpu')
+
+    env = make_vec_envs(
+        lambda env_id: lambda: gen_env_with_seed(config, 11),
+        num_processes=1,
+        device='cpu'
+    )
+
+    rewards, steps, successes = [], [], []
+    for _ in trange(args.episodes):
+        obs, done, episode_reward = env.reset(), False, 0
+        episode_steps = 0
+        rnn_hxs = torch.zeros((1, config.get('encoder.recurrent_hidden_size', 1)))
+        masks = torch.zeros((1, 1))
+
+        while not done:
+            if args.viz:
+                env.render('human')
+            value, action, _, rnn_hxs = agent.act(obs, rnn_hxs, masks, deterministic=True)
+            # observation, reward and next obs
+            obs, reward, done, _ = env.step(action)
+            episode_reward += float(reward)
+            episode_steps += 1
+        rewards.append(episode_reward)
+        steps.append(episode_steps)
+        successes.append(episode_reward > 0)
+        if args.viz:
+            input(f'> the end!, reward = {episode_reward}')
+
+    logger.info(f'mode {args.mode}: '
+                f'reward {np.mean(rewards):.2f}, '
+                f'steps {np.mean(steps):.2f}, '
+                f'success {np.mean(successes):.2f}')
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--mode', choices=['worker', 'master', 'ssim_master', 'ssim_worker', 'direct_ppo', 'rnd_ppo', 'multi', 'monte'])
+    parser.add_argument('--viz', action='store_true')
+    parser.add_argument('--episodes', default=100, type=int)
+    args = parser.parse_args()
+
+    init_logger(__name__)
+    main(args)
